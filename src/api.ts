ca662c956efbd0288737d9880c104040a70703ab
xmmmmmovo <13256172360@163.com>
2020-02-07 18:21:55 +0800

    fetchdata

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,18 @@
 import axios from 'axios'
+import {config} from "./app";
 
 /**网络请求 */
 export const remote_get = function (url: string) {
     const promise = new Promise(function (resolve, reject) {
-        axios.get(url).then((res: any) => {
+        axios.get(
+            url,
+            {
+                headers: {
+                    'Cookie': config['Cookie'],
+                    'User-Agent': config['User-Agent']
+                }
+            }
+        ).then((res: any) => {
             resolve(res.data);
         }, (err: any) => {
             reject(err);
--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,64 @@ import {remote_get} from './api'
 import * as cheerio from 'cheerio'
 import {sleep} from "./util";
 import jsyaml from "js-yaml"
-import {readFileSync} from "fs";
+import {existsSync, readFileSync, writeFileSync} from "fs";
+import {stringify} from "querystring";
 
 export let config: any
+let urls: string[] = [];
+let titles: string[] = [];
+
 const init = async () => {
     try {
         config = jsyaml.safeLoad(
             readFileSync('config.yaml', 'utf8')
-        )
+        ).app
         console.log(config)
     } catch (e) {
         console.log(e)
     }
 }
 
-const fetchData = async () =>{
-    await remote_get(config['url'])
+const fetchData = async () => {
+    const res: any = await remote_get(config['url'])
+    // console.log(res)
+    const $ = cheerio.load(res);
+    // 获取网页中的数据，分别写到两个数组里面
+    $('.hide-div')
+        .children()
+        .each((index, element) => {
+            titles.push(
+                // @ts-ignore
+                $(element)
+                    .find('.res-info')
+                    .find('.overflow-ellipsis')
+                    .find('span')
+                    .attr('title')
+                    .trim()
+            );
+            // @ts-ignore
+            urls.push($(element).attr('data-href').trim());
+        })
+    // 打印数组
+    console.log(titles, urls);
+    writeFileSync('cache/title.txt', titles)
+    writeFileSync('cache/urls.txt', urls)
 }
 
-init()
+const main = async () => {
+    await init()
+    if (
+        existsSync('cache/title.txt') &&
+        existsSync('cache/urls.txt')
+    ) {
+        urls = readFileSync('cache/urls.txt')
+            .toString().split(',')
+        titles = readFileSync('cache/title.txt')
+            .toString().split(',')
+        console.log(titles, urls)
+    } else {
+        await fetchData()
+    }
+}
+
+main()
