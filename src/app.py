1f07bd558c16876cdea6de96185b4875b9350e85
xmmmmmovo <13256172360@163.com>
2020-02-09 18:29:19 +0800

    add

---
+++ src/app.py
@@ -0,0 +1,140 @@
+import os
+import asyncio
+from pprint import pprint
+import requests
+import yaml
+from pyquery import PyQuery
+
+from src.video import Mp4info
+
+config = None
+titles = []
+urls = []
+res_id = []
+loop = asyncio.get_event_loop()
+
+
+def get(url, params=None):
+    resp = requests.get(url=url, params=params, headers={
+        'Cookie': config['url_config']['Cookie'],
+        'User-Agent': config['url_config']['User-Agent'],
+    })
+    return resp.content if resp.status_code == 200 else None
+
+
+def post(url, data=None):
+    resp = requests.post(
+        url=url,
+        data=data,
+        headers={
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Cookie': config['url_config']['Cookie'],
+            'User-Agent': config['url_config']['User-Agent'],
+        }
+    )
+    return resp.content if resp.status_code == 200 else None
+
+
+def file_write(name: str, data: list):
+    file = open(f'../cache/{name}.txt', 'w', encoding='utf8')
+    file.write('\n'.join(data))
+    file.close()
+
+
+def config_load():
+    global config
+    config = yaml.load(
+        open(r'../config.yaml', encoding='utf8'),
+        Loader=yaml.FullLoader
+    )
+
+
+def get_course_src():
+    resp_text = get(
+        config['url_config']['base_url'] + config['app']['crouse_id']
+    )
+    parser = PyQuery(resp_text)
+
+    def fun_iter(i, element):
+        dataIsDrag = parser(element) \
+            .find('.res-info') \
+            .find('.create-box') \
+            .children()[-4] \
+            .attrib['data-is-drag']
+
+        if dataIsDrag == 'N':
+            titles.append(
+                parser(element)
+                    .find('.res-info')
+                    .find('.overflow-ellipsis')
+                    .find('.res-name')
+                    .text()
+                    .strip()
+            )
+            urls.append(
+                parser(element)
+                    .attr('data-href')
+                    .strip()
+            )
+            res_id.append(
+                parser(element)
+                    .attr('data-value')
+                    .strip()
+            )
+
+    parser('.hide-div') \
+        .children() \
+        .each(fun_iter)
+
+    file_write('urls', urls)
+    file_write('titles', titles)
+    file_write('res_id', res_id)
+
+
+async def video_score(i):
+    file = Mp4info(urls[i])
+    d = file.get_duration()
+    pprint(d)
+    data = {
+        'clazz_course_id': '432F3210-2FA6-11EA-BA85-506B4B4A8386',
+        'res_id': res_id[i],
+        'watch_to': int(d) + 1,
+        'duration': int(d) + 1,
+        'current_watch_to': 0
+    }
+    resp = post(
+        url=config['url_config']['video_url'],
+        data=data
+    )
+    pprint(str(i) + str(resp))
+
+
+async def other_score(i):
+    resp = get(url=urls[i])
+    pprint(str(i) + str("normal_complete"))
+
+
+async def score_async(i):
+    ext = os.path.splitext(titles[i])[1]
+    if ext == '.mp4':
+        await video_score(i)
+    else:
+        await other_score(i)
+
+
+def score_main():
+    rate = int(config['app']['exp_speed'] / 2)
+    for i in range(rate):
+        loop.run_until_complete(score_async(i))
+
+
+if __name__ == '__main__':
+    pprint('--------读取配置文件--------')
+    config_load()
+    pprint(config)
+    pprint('--------读取成功--------')
+    pprint('--------获取网课资源列表--------')
+    get_course_src()
+    pprint('--------获取成功--------')
+    pprint('--------开始刷资源--------')
+    score_main()
---
+++ src/video.py
@@ -0,0 +1,53 @@
+import requests, struct
+from pprint import pprint
+
+
+class Mp4info:
+    def __init__(self, file):
+        self.file = file
+        self.seek = 0
+        self.duration = 0
+        self.s = requests.session()
+        self.timeout = 30
+        self.s.headers = {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Cookie': '_uab_collina=158099356218517362584412; acw_tc=707c9f9d15809935624926727e3de40a58560ba6a44dd4d3cab4b8d21211cc; login_token=4bc891c38b87c3048edb8b59493b42f725176e3408875bc1e96a5fc0109b47da; teachweb=d4f4172703f4ec24a7f6c1a4c16efa458b4d0b74; SERVERID=da350d2ba570698afee980c284304ee2|1581183791|1581183764',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'
+        }
+
+    # 设置请求头
+    def _set_headers(self, seek):
+        self.s.headers['Range'] = 'bytes={}-{}'.format(seek, seek + 7)
+
+    def _send_request(self):
+        try:
+            data = self.s.get(url=self.file, stream=True,
+                              timeout=self.timeout).raw.read()
+        except requests.Timeout:
+            raise self.file + '连接超时:超过30秒(默认)服务器没有响应任何数据！'
+        return data
+
+    def _find_moov_request(self):
+        self._set_headers(self.seek)
+        data = self._send_request()
+        size = int(struct.unpack('>I', data[:4])[0])
+        flag = data[-4:].decode('ascii')
+        return size, flag
+
+    def _find_duration_request(self):
+        # 4+4是moov的大小和标识,跳过20个字符，直接读到time_scale，duration
+        self._set_headers(seek=self.seek + 4 + 4 + 20)
+        data = self._send_request()
+        time_scale = int(struct.unpack('>I', data[:4])[0])
+        duration = int(struct.unpack('>I', data[-4:])[0])
+        return time_scale, duration
+
+    def get_duration(self):
+        while True:
+            size, flag = self._find_moov_request()
+            if flag == 'moov':
+                time_scale, duration = self._find_duration_request()
+                self.duration = duration / time_scale
+                return self.duration
+            else:
+                self.seek += size
